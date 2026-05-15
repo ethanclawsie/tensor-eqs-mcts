2c20bb0239e41ac388292fe4298105926dbf0bb0
ethanclawsie <ethanclawsie@gmail.com>
2026-05-14 23:11:20 -0700

    add new variations

--- tensat/run_exp_main.sh
+++ tensat/run_exp_main.sh
@@ -16,7 +16,7 @@ extract="ilp" # greedy, egg_ilp, ilp
 
 n_iter=10000 # iter_limit: Max number of iterations for egg to run
 n_sec=3600 # time_limit: Max number of seconds for egg to run
-n_nodes=2000 # n_nodes: Max number of nodes for egraph
+n_nodes=4000 # n_nodes: Max number of nodes for egraph
 ilp_time_sec=600 # Time limit for ILP solver (0 = no limit)
 ilp_num_threads=1 # Number of threads for ILP solver
 node_multi=2000 # Max number of nodes added by multi-pattern rules
@@ -39,15 +39,7 @@ num_passes=5
 
 # Neural network architectures
 models=(
-    inceptionv3
-    resnext50
-    bert
-    nasneta
     squeezenet
-    vgg
-    mobilenetv2
-    resnet50
-    nasrnn
 )
 
 # k_multi settings for each model
@@ -66,14 +58,42 @@ declare -A models_iter_multi=(
 # export TASO_GPU_WARMUP=0
 # set -e
 
+best_runtime=""
+best_trial=""
+
 for pass in $(seq 0 $(expr $num_passes - 1)); do
     for model in "${models[@]}"; do
+        trial_num=$((pass + 1))
         iter_multi=${models_iter_multi[$model]}
         if [ "$model" == "nasrnn" ]; then
             multi_rules=$multi_rules_nasrnn
         else
             multi_rules=$multi_rules_default
         fi
-        cargo run --release -- --iter_multi $iter_multi --model $model --mode $mode --rules "$rules" --multi_rules "$multi_rules" --out_file $out_file -s $save_graph --n_iter $n_iter --n_sec $n_sec --n_nodes $n_nodes --ilp_time_sec $ilp_time_sec --extract $extract --ilp_num_threads $ilp_num_threads --node_multi $node_multi --output_dir "$EXPERIMENTS_DIR/tensat/${model}_${iter_multi}_${pass}" $export_models$use_multi$no_order$all_weight_only$no_cycle$order_var_int$class_constraint$initial_with_greedy$filter_before$saturation_only
+
+        printf '\n=== Trial %d/%d | model=%s | iter_multi=%s ===\n' "$trial_num" "$num_passes" "$model" "$iter_multi"
+
+        tmp_output=$(mktemp)
+        set +e
+        RUSTFLAGS="${RUSTFLAGS:-} -Awarnings" cargo run --quiet --release -- --iter_multi $iter_multi --model $model --mode $mode --rules "$rules" --multi_rules "$multi_rules" --out_file $out_file -s $save_graph --n_iter $n_iter --n_sec $n_sec --n_nodes $n_nodes --ilp_time_sec $ilp_time_sec --extract $extract --ilp_num_threads $ilp_num_threads --node_multi $node_multi --output_dir "$EXPERIMENTS_DIR/tensat/${model}_${iter_multi}_${pass}" $export_models$use_multi$no_order$all_weight_only$no_cycle$order_var_int$class_constraint$initial_with_greedy$filter_before$saturation_only 2>&1 | tee "$tmp_output"
+        status=${PIPESTATUS[0]}
+        set -e
+        if [ "$status" -ne 0 ]; then
+            rm -f "$tmp_output"
+            exit "$status"
+        fi
+
+        current_runtime=$(awk '/Extracted graph runtime:/ { value = $4 } END { print value }' "$tmp_output")
+        rm -f "$tmp_output"
+        if [ -n "$current_runtime" ]; then
+            if [ -z "$best_runtime" ] || awk "BEGIN { exit !($current_runtime < $best_runtime) }"; then
+                best_runtime=$current_runtime
+                best_trial=$trial_num
+            fi
+
+            printf 'Summary: trial %d current=%s best_so_far=%s (trial %s)\n' "$trial_num" "$current_runtime" "$best_runtime" "$best_trial"
+        else
+            printf 'Summary: trial %d current=unknown best_so_far=%s\n' "$trial_num" "${best_runtime:-unknown}"
+        fi
     done
 done
